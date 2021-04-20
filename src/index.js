5fdfc0688c2994c25fba1b13ec8c5eee65d5d345
benyafang.arya <benyafang.arya@bytedance.com>
2021-04-20 18:30:19 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.less';
+
+const App = () => (
+  <div className="test">
+    Hello World！dfafdsfs
+  </div>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'));
